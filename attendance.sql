a8c8c85fb5ba4ec526ca01a1fc05ae32debf7455
TusharTevatia <sidtevatia@gmail.com>
2024-09-16 21:59:54 +0530

    Updated comments

--- attendance.sql
+++ attendance.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.0.4
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: May 12, 2014 at 05:39 PM
--- Server version: 5.6.12-log
--- PHP Version: 5.4.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
